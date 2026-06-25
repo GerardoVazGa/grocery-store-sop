02f9bb6febb6b47c273225ee33bf9f0ccec03403
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-06-25 13:25:01 -0600

    feat(main): add main window system with centralized paths and enviroment variables

---
+++ electron/main/index.js
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from "electron"
+import { paths } from "../configs/paths.js"
+import { DEV_SERVER_URL } from "../configs/env.js"
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: paths.preload,
+            contextIsolation: true,
+            nodeIntegration: false
+        },
+    })
+
+    const isDev = !app.isPackaged
+
+    if (isDev) {
+        mainWindow.loadURL(DEV_SERVER_URL)
+        mainWindow.webContents.openDevTools()
+    } else {
+        mainWindow.loadFile(paths.distIndex)
+    }
+}
+
+app.whenReady().then(createWindow)
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+})
