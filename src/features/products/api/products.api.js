c845e82aed1a5856573d62a737b07c12a9de0491
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-06-29 14:42:58 -0600

    feat(products): add products ipc api

---
+++ src/features/products/api/products.api.js
@@ -0,0 +1,7 @@
+export const productsApi = {
+    getAll: async () => window.api.products.getAll(),
+    findByBarCode: async (barCode) => window.api.products.findByBarCode(barCode),
+    findById: async (id) => window.api.products.findById(id),
+    create: async (product) => window.api.products.create(product),
+    update: async (id, product) => window.api.products.update(id, product)
+}
