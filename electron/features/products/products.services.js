c818e6da6e1fad2463a974e42615b3e452a5c966
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-06-29 18:30:08 -0600

    feat(products): add services layer with field and business rule validations

---
+++ electron/features/products/products.services.js
@@ -0,0 +1,65 @@
+import { findBrandById } from "../brands/brands.repository.js"
+import { findCategoryById } from "../categories/categories.repository.js"
+import { createProduct, findProductByBarCode, updateProduct } from "./products.repository.js"
+
+function validateProduct(db, product, productIdBeingUpdated = null) {
+    const { barcode, name, categoryId, brandId, price, cost, stock } = product
+
+    if(!name || name.trim().length === 0) {
+        throw new Error("El nombre del producto es requerido")
+    }
+
+    if(typeof price !== "number" || price <= 0) {
+        throw new Error("El precio debe ser mayor a 0")
+    }
+
+    if(typeof cost !== "number" || cost < 0) {
+        throw new Error("El costo no puede ser negativo")
+    }
+
+    if (typeof stock !== "number" || stock < 0 || !Number.isInteger(stock)) {
+        throw new Error("El stock debe ser un número entero no negativo")
+    }
+
+
+    const barcodeClean = barcode.trim() || null
+
+    if(barcodeClean) {
+        const exist = findProductByBarCode(barcodeClean)
+
+        if(exist && exist.id !== productIdBeingUpdated) {
+            throw new Error(`Ya existe un producto con el código de barras ${barcodeClean}`)
+        }
+    }
+
+    if(categoryId) {
+        const category = findCategoryById(db, categoryId)
+
+        if(!category) {
+            throw new Error("La categoría seleccionada no existe")
+        }
+    }
+
+    if(brandId) {
+        const brand = findBrandById(db, brandId)
+
+        if(!brand) {
+            throw new Error("La marca seleccionada no existe")
+        }
+
+        if(brand.categoryId !== categoryId) {
+            throw new Error("La marca seleccionada no pertenece a la categoría seleccionada")
+        }
+    }
+
+}
+
+export function createProductService(db, product) {
+    validateProduct(db, product)
+    return createProduct(db, product)
+}
+
+export function updateProductService(db, id, product) {
+    validateProduct(db, product, id)
+    return updateProduct(db, id, product)
+}
