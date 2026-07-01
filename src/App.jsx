ba7345cdc955b2229c21f05c66df7ec93bcf7fd3
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-07-01 11:36:35 -0600

    feat(product): implement ProductForm component with hadler create and loading condition to show ProductList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
+import { ProductForm } from './features/products/components/ProductsForm.jsx'
 import { ProductsList } from './features/products/components/ProductsList.jsx'
+import { useProducts } from './features/products/hooks/useProducts.js'
 
 function App() {
+  const { products, isLoading, createProduct } = useProducts()
 
+  const handleCreate = async (product) => {
+    await createProduct(product)
+  }
+  
   return (
     <div>
       <h1>Productos</h1>
-      <ProductsList />
+      <ProductForm onCreate={handleCreate}/>
+      <hr/>
+      {
+        isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <ProductsList products={products} />
+        )
+      }
+
     </div>
   )
 }
