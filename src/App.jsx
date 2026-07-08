8c87a42eeb6ab33475d431551bc6c3c9948f935b
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-07-08 10:29:42 -0600

    refactor(app): mount AppRouter as root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
 import './App.css'
-import { ProductForm } from './features/products/components/ProductsForm.jsx'
-import { ProductsList } from './features/products/components/ProductsList.jsx'
-import { useProducts } from './features/products/hooks/useProducts.js'
+import { AppRouter } from './app/navigation/AppRouter.jsx'
 
 function App() {
-  const { products, isLoading, createProduct } = useProducts()
-
-  const handleCreate = async (product) => {
-    await createProduct(product)
-  }
-  
   return (
-    <div>
-      <h1>Productos</h1>
-      <ProductForm onCreate={handleCreate}/>
-      <hr/>
-      {
-        isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          <ProductsList products={products} />
-        )
-      }
-
-    </div>
+    <AppRouter />
   )
 }
 
