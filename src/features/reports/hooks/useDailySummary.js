1c6aa4a479da698db414075577f60decc59db5e4
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-07-08 18:03:41 -0600

    feat(reports): add useDailySummary hook

---
+++ src/features/reports/hooks/useDailySummary.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { reportsApi } from "../api/reports.api";
+
+export function useDailySummary() {
+    const [dailySummary, setDailySummary] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchDailySummary = async() => {
+            try {
+                setIsLoading(true)
+                const data = await reportsApi.getDailySummary()
+                setDailySummary(data)
+                setError(null)
+            } catch (error) {
+                setError(error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        fetchDailySummary()
+    }, [])
+
+    return { dailySummary, isLoading, error }
+}
