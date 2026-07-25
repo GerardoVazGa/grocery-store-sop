30296918b261aad92d471d72f93a5421058dea79
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-07-24 19:19:34 -0600

    feat(cashCuts): add getDailySales and close to cashCuts API layer

--- src/features/cashCuts/api/cashCuts.api.js
+++ src/features/cashCuts/api/cashCuts.api.js
@@ -1,3 +1,5 @@
 export const cashCutsApi = {
-    getSummary: async() => window.api.cashCuts.getSummary()
+    getSummary: async() => window.api.cashCuts.getSummary(),
+    getDailySales: async() => window.api.cashCuts.getDailySales(),
+    closeCashCut: async() => window.api.cashCuts.close()
 }
