bb6926ba4cf8893a8fb8edc1e896d84c165f7ebe
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-07-24 19:09:42 -0600

    feat(cashCuts): add getDailySales and close IPC handlers

--- electron/features/cashCuts/cashCuts.handlers.js
+++ electron/features/cashCuts/cashCuts.handlers.js
@@ -1,9 +1,17 @@
 import { ipcMain } from "electron";
-import { getCashCutSummary } from "./cashCuts.repository.js";
+import { closeCashCut, getCashCutSummary, getDailySales } from "./cashCuts.repository.js";
 import db from "../../db/connection.js";
 
 export function registerCashCutsHandlers() {
     ipcMain.handle("cashCuts:getSummary", () => {
         return getCashCutSummary(db)
+    })
+
+    ipcMain.handle("cashCuts:getDailySales", () => {
+        return getDailySales(db)
+    })
+
+    ipcMain.handle("cashCuts:close", () => {
+        return closeCashCut(db)
     })
 }
