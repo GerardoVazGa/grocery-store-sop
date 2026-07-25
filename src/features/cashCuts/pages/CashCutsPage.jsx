b109a119835e5dfca09f39c16a00f39cedd218fc
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-07-24 20:49:55 -0600

    feat(cashCuts): implement summary, sales history and close button into CashCutsPage

--- src/features/cashCuts/pages/CashCutsPage.jsx
+++ src/features/cashCuts/pages/CashCutsPage.jsx
@@ -1,10 +1,41 @@
 import { CashCutSummary } from "../components/CashCutSummary";
+import { CloseCashCutButton } from "../components/CloseCashCutButton";
+import { DailySalesList } from "../components/DailySalesList";
+import { useCashCutsSummary } from "../hooks/useCashCutsSummary";
+import { useDailySales } from "../hooks/useDailySales";
+import { useCashCutStore } from "../store/cashCutStore";
 
 export function CashCutsPage() {
+    const { cashCutsSummary, isLoading: isLoadingCashCutsSummary } = useCashCutsSummary()
+    const { dailySales, isLoading: salesLoading } = useDailySales()
+    const { closeCashCut, isClosing, error, isClosed } = useCashCutStore()
+
+    const isLoading = isLoadingCashCutsSummary || salesLoading
+
+    if (isLoading) return <p>Cargando Corte de caja...</p>
+
+    if (isClosed) {
+        return (
+            <div>
+                <h1>Corte de caja</h1>
+                <p>✅ El corte del día se cerró correctamente.</p>
+            </div>
+        )
+    }
+
+
     return (
         <div>
-            <h1>Cortes de caja</h1>
-            <CashCutSummary />
+            <h1>Corte de caja</h1>
+            <CashCutSummary cashCutsSummary={cashCutsSummary} />
+            <hr />
+            <DailySalesList sales={dailySales} />
+            <hr />
+            <CloseCashCutButton
+                onClose={closeCashCut}
+                isLoading={isClosing}
+                error={error}
+            />
         </div>
     )
 }
