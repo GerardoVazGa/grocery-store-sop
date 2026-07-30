817bc64c591eba863e43bcfe6182913e12f6fe68
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-07-30 11:22:04 -0600

    feat(brands): add create, update, findById and findWithCategoryId IPC handlers

--- electron/features/brands/brands.handlers.js
+++ electron/features/brands/brands.handlers.js
@@ -1,6 +1,7 @@
 import { ipcMain } from "electron"
 import db from "../../db/connection.js"
-import { getAllBrands, getBrandsByCategory } from "./brands.repository.js"
+import { findBrandById, findBrandWithCategoryId, getAllBrands, getBrandsByCategory } from "./brands.repository.js"
+import { createBrandService, updateBrandService } from "./brands.services.js"
 
 export function registerBrandsHandlers() {
     ipcMain.handle("brands:getAll", () => {
@@ -9,5 +10,21 @@ export function registerBrandsHandlers() {
 
     ipcMain.handle("brands:getByCategory", (event, categoryId) => {
         return getBrandsByCategory(db, categoryId)
+    })
+
+    ipcMain.handle("brands:findById", (event, id) => {
+        return findBrandById(db, id)
+    })
+
+    ipcMain.handle("brands:findWithCategoryId", (event, name, categoryId) => {
+        return findBrandWithCategoryId(db, name, categoryId)
+    })
+
+    ipcMain.handle("brands:create", (event, data) => {
+        return createBrandService(db, data)
+    })
+
+    ipcMain.handle("brands:update", (event, id, data) => {
+        return updateBrandService(db, id, data)
     })
 }
