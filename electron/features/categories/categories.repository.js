0b1819d24dc54ec400932822bc5fcfca4035f862
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-07-30 11:14:47 -0600

    feat(categories): add createCategory, updateCategory and findCategoryByName queries

--- electron/features/categories/categories.repository.js
+++ electron/features/categories/categories.repository.js
@@ -10,6 +10,17 @@ export function getAllCategories(db) {
     ).all()
 }
 
+export function findCategoryByName(db, name) {
+    return db.prepare(
+        `
+            SELECT 
+                categories.id
+            FROM categories
+            WHERE categories.name = ?
+        `
+    ).get(name)
+}
+
 export function findCategoryById(db, id) {
     return db.prepare(
         `
@@ -20,4 +31,36 @@ export function findCategoryById(db, id) {
             WHERE categories.id = ?
         `
     ).get(id)
+}
+
+export function createCategory(db, name) {
+    const result = db.prepare(
+        `
+            INSERT INTO categories (name)
+            VALUES (?)
+        `
+    ).run(name)
+
+    return {
+        id: result.lastInsertRowid
+    }
+}
+
+export function updateCategory(db, id, name) {
+    const result = db.prepare(
+        `
+            UPDATE categories
+            SET name = ?
+            WHERE id = ?
+        `
+    ).run(name, id)
+
+    if(result.changes === 0) {
+        throw new Error("Category with id: " + id + " not found")
+    }
+
+    return {
+        id,
+        name
+    }
 }
