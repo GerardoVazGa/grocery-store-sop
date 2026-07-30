d12a9676e233da2fbed1c8f0daad975f109717da
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-07-30 11:17:46 -0600

    feat(categories): add create, update, findById and findByName IPC handlers

--- electron/features/categories/categories.handlers.js
+++ electron/features/categories/categories.handlers.js
@@ -1,9 +1,26 @@
 import { ipcMain } from "electron"
-import { getAllCategories } from "./categories.repository.js"
+import { findCategoryById, findCategoryByName, getAllCategories } from "./categories.repository.js"
 import db from "../../db/connection.js"
+import { createCategoryService, updateCategoryService } from "./categories.services.js"
 
 export function registerCategoriesHandlers() {
     ipcMain.handle("categories:getAll", () => {
         return getAllCategories(db)
+    })
+
+    ipcMain.handle("categories:findById", (event, id) => {
+        return findCategoryById(db, id)
+    })
+
+    ipcMain.handle("categories:findByName", (event, name) => {
+        return findCategoryByName(db, name)
+    })
+
+    ipcMain.handle("categories:update", (event, id, data) => {
+        return updateCategoryService(db, id, data)
+    })
+
+    ipcMain.handle("categories:create", (event, data) => {
+        return createCategoryService(db, data)
     })
 }
