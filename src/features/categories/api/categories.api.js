12ebab8c414242a67da4e7bd444fba6363cf71e9
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-08-03 20:38:22 -0600

    feat(categories): add categories API layer

--- src/features/categories/api/categories.api.js
+++ src/features/categories/api/categories.api.js
@@ -1,3 +1,6 @@
 export const categoriesApi = {
     getAll: async () => window.api.categories.getAll(),
+    findById: async (id) => window.api.categories.findById(id),
+    create: async (data) => window.api.categories.create(data),
+    update: async (id, data) => window.api.categories.update(id, data),
 }
