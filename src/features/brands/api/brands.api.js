ac1097a18eebefc221865431865ea7dcd0658b89
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-08-03 20:39:35 -0600

    feat(brands): add brands API layer

--- src/features/brands/api/brands.api.js
+++ src/features/brands/api/brands.api.js
@@ -1,4 +1,8 @@
 export const brandsApi = {
     getAll: async () => window.api.brands.getAll(),
     getByCategory: async (categoryId) => window.api.brands.getByCategory(categoryId),
+    findById: async (id) => window.api.brands.findById(id),
+    findWithCategoryId: async (name, categoryId) => window.api.brands.findWithCategoryId(name, categoryId),
+    create: async (data) => window.api.brands.create(data),
+    update: async (id, data) => window.api.brands.update(id, data),
 }
