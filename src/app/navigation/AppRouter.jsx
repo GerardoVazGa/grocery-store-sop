66a5ed6d8a7c233e854cb18fef1ee989dccfc15e
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-08-03 20:42:56 -0600

    feat(navigation): add settings routes with categories and brands

--- src/app/navigation/AppRouter.jsx
+++ src/app/navigation/AppRouter.jsx
@@ -4,6 +4,9 @@ import { ProductsPage } from "../../features/products/pages/ProductsPage";
 import { DashboardPage } from "../../features/reports/pages/DashBoardPage";
 import { CashCutsPage } from "../../features/cashCuts/pages/CashCutsPage";
 import { SalesPage } from "../../features/sales/pages/salesPage";
+import { SettingsPage } from "../../features/settings/pages/SettingsPage";
+import { CategoriesPage } from "../../features/categories/pages/CategoriesPage";
+import { BrandsPage } from "../../features/brands/pages/BrandsPage";
 
 export function AppRouter() {
     return (
@@ -14,6 +17,10 @@ export function AppRouter() {
                     <Route path="products" element={<ProductsPage />} />
                     <Route path="sales" element={<SalesPage />} />
                     <Route path="cash-cuts" element={<CashCutsPage />} />
+                    <Route path="settings" element={<SettingsPage />} >
+                        <Route path="categories" element={<CategoriesPage />} />
+                        <Route path="brands" element={<BrandsPage />} />
+                    </Route>
                 </Route>
             </Routes>
         </MemoryRouter>
