1aeac63a3f98275eaff76be90fad746669c5ace9
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-08-10 20:10:01 -0600

    feat(products): add delete to products API layer

--- src/features/products/api/products.api.js
+++ src/features/products/api/products.api.js
@@ -4,5 +4,6 @@ export const productsApi = {
     findById: async (id) => window.api.products.findById(id),
     create: async (product) => window.api.products.create(product),
     update: async (id, product) => window.api.products.update(id, product),
+    delete: async (id) => window.api.products.delete(id),
     search: async (query) => window.api.products.search(query),
 }
