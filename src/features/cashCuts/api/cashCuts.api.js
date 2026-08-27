b5e22240e3ff0de0326d9baa31059fd5d83afa7a
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-08-26 18:20:46 -0600

    feat(cashCuts): update API layer with open, close, reopen and getActive methods

--- src/features/cashCuts/api/cashCuts.api.js
+++ src/features/cashCuts/api/cashCuts.api.js
@@ -1,5 +1,9 @@
 export const cashCutsApi = {
     getSummary: async() => window.api.cashCuts.getSummary(),
     getDailySales: async() => window.api.cashCuts.getDailySales(),
-    closeCashCut: async() => window.api.cashCuts.close()
+    getActiveCashCut: async() => window.api.cashCuts.getActive(),
+    openCashCut: async(openingAmount) => window.api.cashCuts.open(openingAmount),
+    reopenCashCut: async(cashCutId) => window.api.cashCuts.reopen(cashCutId),
+    getCashCutById: async(id) => window.api.cashCuts.getById(id),
+    closeCashCut: async(cashCutId, countedCash) => window.api.cashCuts.close(cashCutId, countedCash)
 }
