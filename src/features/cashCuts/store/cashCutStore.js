f37167f1c3f7b65d2228c70642876d4dee7088a0
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-08-26 18:21:55 -0600

    feat(cashCuts): update store with open, close, reopen (reset) and checkActiveCashCut flow

--- src/features/cashCuts/store/cashCutStore.js
+++ src/features/cashCuts/store/cashCutStore.js
@@ -1,22 +1,69 @@
 import { create } from "zustand"
 import { cashCutsApi } from "../api/cashCuts.api.js"
 
-export const useCashCutStore = create((set) => ({
+export const useCashCutStore = create((set, get) => ({
+    activeCashCut: null,
     isClosed: false,
-    isClosing: false, // isLoading state
+    isLoading: true,
+    isOpening: false,
+    isClosing: false,
+    isReopening: false,
     error: null,
+    lastClosedCashCutId: null,
 
-    closeCashCut: async () => {
+    checkActiveCashCut: async () => {
         try {
-            set({ isClosing: true, error: null });
-            await cashCutsApi.closeCashCut();
-            set({ isClosed: true});
+            const activeCashCut = await cashCutsApi.getActiveCashCut()
+            set({activeCashCut, isLoading: false})
+
         } catch (error) {
-            set({ error: error.message });
-        } finally {
-            set({ isClosing: false });
+            set({ error: error.message, isLoading: false })
         }
     },
 
-    reset: () => set({ isClosed: false, error: null }) // reset closeCashCut
+    openCashCut: async (openingAmount) => {
+        try {
+            set({ isOpening: true, error: null })
+            const activeCashCut = await cashCutsApi.openCashCut(openingAmount)
+            set({ activeCashCut, isOpening: false, isClosed: false, lastClosedCashCutId: null })
+
+        } catch (error) {
+            set({ error: error.message, isOpening: false })
+        }
+    },
+
+    closeCashCut: async (countedCash) => {
+        try {
+            set({ isClosing: true, error: null })
+            const { activeCashCut } = get()
+            await cashCutsApi.closeCashCut(activeCashCut.id, countedCash)
+            set({ 
+                isClosed: true,
+                lastClosedCashCutId: activeCashCut.id,
+                activeCashCut: null,
+            })
+
+        } catch (error) {
+            set({ error: error.message })
+        } finally {
+            set({ isClosing: false })
+        }
+    },
+
+    reset: async () => {
+        try {
+            const { lastClosedCashCutId } = get()
+
+            if(lastClosedCashCutId) {
+                const activeCashCut = await cashCutsApi.reopenCashCut(lastClosedCashCutId)
+                set({ activeCashCut, isClosed: false, lastClosedCashCutId: null })
+            } else {
+                set({isClosed: false, error: null})
+            }
+        } catch (error) {
+            set({ error: error.message })
+        } finally {
+            set({ isReopening: false })
+        }
+    }
 }))
