eb6f6ced7937ae74ecb8231b7a0c01f22aadce8f
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-09-03 13:19:26 -0600

    refactor(cashCuts): update getSummary  API layer and add new getCashCutSales API layer

--- src/features/cashCuts/api/cashCuts.api.js
+++ src/features/cashCuts/api/cashCuts.api.js
@@ -1,6 +1,6 @@
 export const cashCutsApi = {
-    getSummary: async() => window.api.cashCuts.getSummary(),
-    getDailySales: async() => window.api.cashCuts.getDailySales(),
+    getSummary: async(cashCutId) => window.api.cashCuts.getSummary(cashCutId),
+    getCashCutSales: async(cashCutId) => window.api.cashCuts.getCashCutSales(cashCutId),
     getActiveCashCut: async() => window.api.cashCuts.getActive(),
     openCashCut: async(openingAmount) => window.api.cashCuts.open(openingAmount),
     reopenCashCut: async(cashCutId) => window.api.cashCuts.reopen(cashCutId),
